d8782f241215424dc9b3b0748b362e2f820bb541
Miraaj Khan <miraajk16@gmail.com>
2022-12-29 17:23:56 +0530

    conflict

---
+++ src/main/java/com/rpay/RPayApplication.java
@@ -0,0 +1,13 @@
+package com.rpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rpay/RPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
